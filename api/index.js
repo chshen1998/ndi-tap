d325fe99949504cba7bbbf1a5377442e8611bfbd
chshen1998 <chewhoashen@gmail.com>
2022-09-08 14:38:25 +0800

    Connect to local mongodb

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+
+const app = express();
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors()) // config cors so that front-end can use
+app.options('*', cors())
+
+const httpServer = createServer(app)
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+httpServer.listen(8000);
