228949e561d7515417dc3f0b2b1464a9d90cbe6a
chshen1998 <chewhoashen@gmail.com>
2022-09-08 16:00:41 +0800

    Setup web sockets

--- api/controller/info-request-controller.js
+++ api/controller/info-request-controller.js
@@ -1,11 +1,18 @@
+import uniqid from 'uniqid'
 import { createInfoRequest } from "../service/info-request-service.js";
 
-export async function handleCreateInfoRequest(req, res) {
-    const body = req.body
-    const infoReq = await createInfoRequest(body["requester"])
+export async function handleOpenInfoRequest(socket, requester) {
+    const infoReq = await createInfoRequest(requester)
+
     if (infoReq.err) {
-        return res.status(400).json({message:"Failed to make info request"})
-    } else {
-        return res.status(200).json({message:"Success"})
+        socket.emit("IRFailed")
+        return
     }
+
+    const roomCode = uniqid()
+    socket.join(roomCode)
+    socket.emit("IRCreated", {
+        roomCode: roomCode
+    })
+    return
 }
--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
-import {handleCreateInfoRequest} from './controller/info-request-controller.js'
+import {Server} from 'socket.io'
+import {handleOpenInfoRequest} from './controller/info-request-controller.js'
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
@@ -9,12 +10,23 @@ app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
 
-const httpServer = createServer(app)
-
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.post('/request', handleCreateInfoRequest)
+const httpServer = createServer(app)
+const io = new Server(httpServer, {})
+
+io.on("connection", (socket) => {
+    console.log("connected " + String(socket.id))
+
+    socket.on("openIR", (requester) => handleOpenInfoRequest(socket, requester))
+
+    //socket.on("joinIR", (requestee, roomCode) => handleJoinInfoRequest(socket, requestee, roomCode))
+
+    socket.on("disconnect", () => {
+        console.log("disconnected " + String(socket.id))
+    })
+})
 
 httpServer.listen(8000);
